a79165075a6078df085c6327a1354f63f1931df6
Trevor Oakley <sales@merrows.co.uk>
2019-03-18 03:27:38 +0000

    var

--- test.c
+++ test.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
-int main(void) {
-  int i;
-  double a,b;
-  char c;
-  int ar[3];
-  a=1.0;
-  c='p';
-  for (i=0;i<3;i++) {
-    ar[i]=i*i+1;
-  }
-  printf("this is my c code");
+void myFunction(int *iptr, double *aptr, char *cptr);
+int main(void)
+{
+  double a;
+  a = 1999999.0;
+  int x = 99;
+  int *xAdr = &x;
+  double *aAddr = &a;
+  int b = 9;
+  char c='p';
+  char *cAddr = &c;
+  printf("my address is = %lf and %p and %zu ", a, aAddr, sizeof(aAddr));
+  printf("my address is = %d and %p and %zu ", x, xAdr, sizeof(xAdr));
+  printf("my address is = %c and %p and %zu ", c, cAddr, sizeof(cAddr));
+
+  //double c = (double)xAdr - (double)aAddr;
+  myFunction(xAdr, aAddr,cAddr);
+
   return 0;
+}
+
+
+void myFunction(int *iptr, double *aptr, char *cptr) {
+  printf("addresses are %p, %p, %p ", iptr, aptr, cptr);
 }
